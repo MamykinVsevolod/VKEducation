7d7627c563dcec825677529d666a738981da59d7
MamykinVsevolod <vsevolodmamykin@gmail.com>
2023-11-15 22:10:44 +0300

    поменял фон, настроил цикл для Index.html

--- WEB/app/views.py
+++ WEB/app/views.py
@@ -4,7 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    questions = [
+        {
+            'id': i,
+            'title': f'Вопрос {i}',
+            'content': 'VK — это более 200 проектов и сервисов, которыми пользуются миллионы. Ты можешь присоединиться к команде, которая их создаёт. Познакомься с нашим офисом и посети виртуальную экскурсию.'
+        } for i in range(10)
+    ]
+    return render(request, 'index.html', {'questions': questions})
 
 
 def question(request):
