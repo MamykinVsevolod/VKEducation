da483beca7bc37232f5c4877a42b41d0b00918c1
MamykinVsevolod <vsevolodmamykin@gmail.com>
2023-11-15 21:25:44 +0300

    2 дз без пагинатора

--- WEB/HomeWork2/urls.py
+++ WEB/HomeWork2/urls.py
@@ -19,7 +19,11 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index),
-    path('question', views.question),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('question', views.question, name='question'),
+    path('ask', views.ask, name='ask'),
+    path('login', views.login, name='login'),
+    path('settings', views.settings, name='settings'),
+    path('signup', views.signup, name='signup'),
+    path('admin/', admin.site.urls)
 ]
