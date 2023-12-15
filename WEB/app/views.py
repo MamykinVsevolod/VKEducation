0d7525ef367a95e6ec16be61304f22cd30d251c9
MamykinVsevolod <vsevolodmamykin@gmail.com>
2023-12-15 20:29:55 +0300

    pagination-hw2-done

--- WEB/app/views.py
+++ WEB/app/views.py
@@ -7,20 +7,28 @@ QUESTIONS = [
         'id': i,
         'title': f'Вопрос {i}',
         'content': 'VK — это более 200 проектов и сервисов, которыми пользуются миллионы. Ты можешь присоединиться к команде, которая их создаёт. Познакомься с нашим офисом и посети виртуальную экскурсию.'
-    } for i in range(100)
+    } for i in range(50)
 ]
 
 
 def paginate(objects, page, per_page=5):
     paginator = Paginator(objects, per_page)
+    obj = paginator.get_page(page)
     # page_items = paginator.page(1).object_list
-    return paginator.page(page)
+    """ return paginator.page(page) """
+    return obj
+
+
+def all_pages(objects, page, per_page=5):
+    paginator = Paginator(objects, per_page)
+    return paginator
 
 
 def index(request, page=1):
     # paginator = Paginator(QUESTIONS, 3)
     # page_items = paginator.page(1).object_list
-    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page), page: page})
+    """return render(request, 'index.html', {'questions': paginate(QUESTIONS, page), page: page})"""
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page), 'paginator': all_pages(QUESTIONS, page)})
 
 
 # return render(request, 'index.html', {'questions': QUESTIONS})
